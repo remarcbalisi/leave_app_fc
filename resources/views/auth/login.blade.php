a9a75d524570f89a912e0df4a79c0aeb811328ca
Remarc Balisi <=>
2018-12-19 03:22:23 +0800

    integrate login layout: OK

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,53 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
-
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<div class="d-flex h-100 align-items-center justify-content-center">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-4 col-sm-6 ml-auto mr-auto">
+        <div class="card">
+            <div class="card-body">
+            <form>
+                <div class="glowBox">
+                <h4 class="title">Log In</h4>
+                <div class="socialMedia">
+                    <a href="#" class="btn btn-just-icon btn-link">
+                                    <i class="fab fa-facebook-square"></i>
+                                </a>
+                    <a href="#" class="btn btn-just-icon btn-link">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                    <a href="#" class="btn btn-just-icon btn-link">
+                                    <i class="fab fa-google-plus"></i>
+                                </a>
                 </div>
+                </div>
+            </form>
+            <div class="desc text-center">
+                <p>Log In now to use our services!</p>
+            </div>
+            <div class="signUp">
+                <!--  <span>
+                                    <input type="text" class="form" placeholder="Name" autocomplete="off" style="cursor: auto;">
+                                    <span class="underline"></span>
+                </span> -->
+                <span>
+                                    <input type="text" class="form" placeholder="Email" autocomplete="off" style="cursor: auto;">
+                                    <span class="underline"></span>
+                </span>
+                <span>
+                                    <input type="password" class="form" placeholder="Password" autocomplete="off" style="cursor: auto;">
+                                    <span class="underline"></span>
+                </span>
+            </div>
+            <div class="text-center">
+                <a href="#" class="btn btn-primary btn-link btn-wd btn-lg">Log In</a>
+            </div>
+            <div class="create"><a href="signup.html">Or create Account</a></div>
             </div>
         </div>
+        </div>
+    </div>
     </div>
 </div>
 @endsection
