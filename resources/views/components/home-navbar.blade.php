b3c70592c2d28bc07fe92e12ccae96b5622cdd70
Regine Laguting <=>
2018-12-20 06:09:59 +0800

    responsive navbar

--- resources/views/components/home-navbar.blade.php
+++ resources/views/components/home-navbar.blade.php
@@ -3,12 +3,14 @@
     <!-- Navbar Logo -->
     <div class="logo">
       <!-- Logo Placeholder for Inlustration -->
-      <img src="{{ asset('images/logo.png') }}" style="float: left;width:50px;height: auto; margin-top: 5px;"> 
+      <img src="{{ asset('images/logo.png') }}", id="logo"> 
+      <p id="fiercecom-text">Fiercecom Inc.</p>
+      <a href="#notification"><img src="{{asset('images/bellicon.png')}}", id="img-icon"></a>
     </div>
 
     <!-- Navbar Links -->
    
-    <ul id="menu">
+    <ul id="menu"> 
       <li><a href="javascript:unhide('toon1');">Apply</a></li>
 	   <li><a href="#services">Services</a></li>
 	   <li><a href="#about">About</a></li>
@@ -18,14 +20,14 @@
 
    
 <!-- Menu Icon -->
-<!-- <div class="menuIcon">
+<div class="menuIcon">
   <span class="icon icon-bars"></span>
   <span class="icon icon-bars overlay"></span>
-</div> -->
+</div>
 
 <div class="overlay-menu">
   <ul id="menu">
-      <li><a  >Apply</a></li>
+      <li><a href="javascript:unhide('toon1');">Apply</a></li>
       <li><a href="#services">Services</a></li>
       <li><a href="#about">About</a></li>
       <li><a href="#contact">Contact</a></li>
