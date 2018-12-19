cd46d476ec66f4fc2868fc8ba6833b38b8b316ff
Remarc Balisi <=>
2018-12-20 04:06:11 +0800

    registration: OK

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,12 +7,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 
 use App\Gender;
 use App\ContactType;
 use App\AddressType;
 use App\Role;
 use App\Department;
+use App\Contact;
+use App\Address;
 
 class RegisterController extends Controller
 {
@@ -67,25 +70,64 @@ class RegisterController extends Controller
         ]);
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
-    protected function create(array $data)
+    public function register(Request $request)
     {
         $check_user = User::where([
-            'fname' => $data['fname'],
-            'mname' => $data['mname'],
-            'lname' => $data['lname'],
+            'fname' => $request->input('fname'),
+            'mname' => $request->input('mname'),
+            'lname' => $request->input('lname'),
+        ])->get();
+
+        if( count($check_user) > 0 ){
+            return redirect()->back()->withErrors([
+                'user_exist' => "User Already Exist!"
+            ]);
+        }
+
+        $new_user = new User;
+        $new_user->fname = $request->input('fname');
+        $new_user->mname = $request->input('mname');
+        $new_user->lname = $request->input('lname');
+        $new_user->b_day = $request->input('b_day');
+        $new_user->gender_id = $request->input('gender');
+        $new_user->date_hired = $request->input('date_hired');
+        $new_user->employee_id = $request->input('employee_id');
+        $new_user->department_id = $request->input('department_id');
+        $new_user->username = $request->input('username');
+        $new_user->email = $request->input('email');
+        $new_user->password = Hash::make($request->input('password'));
+        $new_user->save();
+
+
+        $contact_types = ContactType::get();
+        $address_types = AddressType::get();
+
+        foreach( $contact_types as $contact_type ){
+            $new_contact = new Contact;
+            $new_contact->number = $request->input($contact_type->name . '-number');
+            $new_contact->country_code = $request->input($contact_type->name . '-country-code');
+            $new_contact->contact_type_id = $contact_type->id;
+            $new_contact->user_id = $new_user->id;
+            $new_contact->save();
+        }
+
+        foreach( $address_types as $address_type ){
+            $new_address = new Address;
+            $new_address->street = $request->input($address_type->name . '-street');
+            $new_address->unit_no = $request->input($address_type->name . '-unit-no');
+            $new_address->barangay = $request->input($address_type->name . '-barangay');
+            $new_address->city = $request->input($address_type->name . '-city');
+            $new_address->postal_code = $request->input($address_type->name . '-postal-code');
+            $new_address->province = $request->input($address_type->name . '-province');
+            $new_address->address_type_id = $address_type->id;
+            $new_address->user_id = $new_user->id;
+            $new_address->save();
+        }
+
+        return redirect()->back()->with([
+            'success_msg' => 'Successfuly registered!'
         ]);
 
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
     }
 
     /**
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,6 +8,22 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
+                    @if (session('success_msg'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success_msg') }}
+                        </div>
+                    @endif
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
